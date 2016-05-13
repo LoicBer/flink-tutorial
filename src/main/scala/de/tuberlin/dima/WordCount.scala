67437a5fde88872980a2e60163f760221ae77b23
Loic Bertaux <loic.bertaux@ensta.fr>
2016-05-13 17:05:27 +0200

    state friday afternoon : nothing works :))

--- src/main/scala/de/tuberlin/dima/WordCount.scala
+++ src/main/scala/de/tuberlin/dima/WordCount.scala
@@ -39,9 +39,8 @@ object WordCount {
     val env = ExecutionEnvironment.getExecutionEnvironment
 
     // get input data
-    val text = env.fromElements("To be, or not to be,--that is the question:--",
-      "Whether 'tis nobler in the mind to suffer", "The slings and arrows of outrageous fortune",
-      "Or to take arms against a sea of troubles,")
+    val text = env.readTextFile("data")
+
 
     val counts = text.flatMap { _.toLowerCase.split("\\W+") }
       .map { (_, 1) }
